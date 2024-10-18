ec13db99a918308a7ad09a5fdf9ec0c350c39d18
winroger <52461706+winroger@users.noreply.github.com>
2024-10-18 13:30:37 +0200

    moved stuff to public folder for deployment

--- main.js
+++ main.js
@@ -52,12 +52,12 @@ const sets =
 [
     { 
         name: "Hexagon", 
-        baseurl: "./assets/example_hexa/", 
+        baseurl: "./public/examples/example_hexa/", 
         fileNames: ["HEXA.json"] 
     },
     { 
         name: "Brio Rails", 
-        baseurl: "./assets/example_brio/", 
+        baseurl: "./public/examples/example_brio/", 
         fileNames: ["YSHAPE.json", "STICK.json", "CORNER2.json", "CORNER1.json", "BRIDGE.json"] 
     },
 ];
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  base: '/waspjs/', // Replace with your repository name
+export default ({
+  base: '/waspjs/',
   build: {
     outDir: 'dist',
   },
